32712a027ad29b9afca7ad15fb9f3afb23a91055
ShashwatMishra2411 <mishrashashwat.sharat@gmail.com>
2024-04-08 03:00:59 +0530

    Add background image and custom box shadow to tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,15 @@ export default {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      backgroundImage:{
+        "search": "url('https://s3-alpha-sig.figma.com/img/95a0/f5fc/96ca735c9a72a2c6703970a518c4d6b5?Expires=1713139200&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4&Signature=izHh6Q87H9RW6sV5sct~OfckN7iH~tgo1vwUk2PXd7UeBbm8VOvDOstMB381C~Dc~5~KKWXzuc4L9G718ClV~oe2fAFVBP7H-ChhFIL3QnoKlPW0bCwNxNKkgLfKaVhcf-xzcZDLNq6sydKrAKvvbB-hFrinroPYxc14jnVrhvuLN4VljVdnOSPIXJ1nkymInUJ7802~jOFOtSJRc~fAcyTrFbMLw9vgtNKYvNOUJtSQNSdtvot9IbnAC1x1gkysnArOAxqNrY3czVEp4PIdpGUjmkXolpNkqGwSfdV0W2lSc03Xsh9LuIlQV9bhe3tTSG5s3JkWBjkkhR6rgwHxnA__')",
+      },
+      boxShadow:{
+        'custom': '0 2px 5px 0 rgba(0, 0, 0, 0.5)',
+      
+      }
+    },
   },
   plugins: [],
 }
