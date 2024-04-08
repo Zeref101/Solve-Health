4157e6d2c516b870bbdb726d2b61777e67240e2f
ShashwatMishra2411 <mishrashashwat.sharat@gmail.com>
2024-04-08 19:56:16 +0530

    ✨ Add routes for PrescriptionHistory and DoctorDetails components

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,15 @@
 import React from 'react'
 import { Routes, Route } from 'react-router-dom'
 import StudentDashboard from './StudentDashboard'
+import PrescriptionHistory from './PrescriptionHistory'
+import DoctorDetails from './DoctorDetails'
 
 export default function App() {
   return (
       <Routes>
         <Route path="/" element={<StudentDashboard></StudentDashboard>}></Route>
+        <Route path="/ph" element={<PrescriptionHistory></PrescriptionHistory>}></Route>
+        <Route path="/doc" element={<DoctorDetails></DoctorDetails>}></Route>
       </Routes>
   )
 }
