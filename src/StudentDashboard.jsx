88d86b2af953f4f9ad9f32e1af6be329a842c1be
ShashwatMishra2411 <mishrashashwat.sharat@gmail.com>
2024-04-08 19:55:50 +0530

    🐛 Fix bug in login functionality

--- src/StudentDashboard.jsx
+++ src/StudentDashboard.jsx
@@ -3,17 +3,17 @@ import React from "react";
 export default function StudentDashboard() {
   return (
     <div>
-      <div className="py-4 min-h-screen bg-[#0A171E] w-full text-white flex flex-col gap-4">
+      <div className="py-4 min-h-screen bg-backgroundColor w-full text-white flex flex-col gap-4">
         <div className="flex justify-between items-center p-4">
           <div className="logo flex flex-col justify-center items-center">
             <h1 className=" self-start">Hello,</h1>
             <h1>Emir Husain</h1>
           </div>
           <div className="icon">
-            <div className="bg-black border-2 border-[#D9D9D9] rounded-full w-14 h-14"></div>
+            <div className="bg-black border-2 border-gray-300 rounded-full w-14 h-14"></div>
           </div>
         </div>
-        <div className="quote h-40 bg-[#41515B] mx-6 rounded-2xl flex">
+        <div className="quote h-40 bg-riverBed mx-6 rounded-2xl flex">
           <div className="w-full basis-1/2">
             <img src="" alt="" />
           </div>
@@ -25,7 +25,7 @@ export default function StudentDashboard() {
               I am feeling great right now
             </div>
             <div>
-              <button className="h-full w-full bg-[#20E9C1] rounded-xl text-black py-2 text-lg font-bold shadow-custom">
+              <button className="h-full w-full bg-aquaMarine rounded-xl text-black py-2 text-lg font-bold shadow-custom">
                 Get Started
               </button>
             </div>
@@ -35,7 +35,7 @@ export default function StudentDashboard() {
           <div className="search-bar">
             <input
               type="text"
-              className="w-full h-14 bg-contain bg-no-repeat bg-right bg-[#172935] border-2 bg-search border-[#172935] placeholder:text-white placeholder:font-bold rounded-3xl p-4 text-white"
+              className="w-full h-14 bg-contain bg-no-repeat bg-right bg-dark border-2 bg-search border-dark placeholder:text-white placeholder:font-bold rounded-3xl p-4 text-white"
               placeholder="Search"
             />
           </div>
@@ -47,19 +47,19 @@ export default function StudentDashboard() {
           </div>
           <div className="list flex gap-2 justify-between overflow-x-scroll">
             <div className="flex flex-col justify-center items-center">
-              <div className="h-16 bg-[#172935] hover:bg-[#20E9C1] duration-150 shadow-custom rounded-2xl w-16"></div>
+              <div className="h-16 bg-dark hover:bg-aquaMarine duration-150 shadow-custom rounded-2xl w-16"></div>
               <span className="text-sm">Activity</span>
             </div>
             <div className="flex flex-col justify-center items-center">
-              <div className="h-16 bg-[#172935] hover:bg-[#20E9C1] shadow-custom rounded-2xl w-16"></div>
+              <div className="h-16 bg-dark hover:bg-aquaMarine shadow-custom rounded-2xl w-16"></div>
               <span className="text-sm">Activity</span>
             </div>
             <div className="flex flex-col justify-center items-center">
-              <div className="h-16 bg-[#172935] hover:bg-[#20E9C1] shadow-custom rounded-2xl w-16"></div>
+              <div className="h-16 bg-dark hover:bg-aquaMarine shadow-custom rounded-2xl w-16"></div>
               <span className="text-sm">Activity</span>
             </div>
             <div className="flex flex-col justify-center items-center">
-              <div className="h-16 bg-[#172935] hover:bg-[#20E9C1] shadow-custom rounded-2xl w-16"></div>
+              <div className="h-16 bg-dark hover:bg-aquaMarine shadow-custom rounded-2xl w-16"></div>
               <span className="text-sm">Activity</span>
             </div>
           </div>
@@ -71,9 +71,9 @@ export default function StudentDashboard() {
           </div>
           <div className="list py-4 px-2 flex gap-10 justify-between overflow-x-scroll">
             <div className="flex flex-col justify-center items-center">
-              <div className="flex flex-col justify-center items-center h-36 bg-[#172935] hover:scale-105 duration-150 ease-in-out rounded-2xl shadow-custom w-24">
-                <div className="h-20 w-20 rounded-full border-4 border-[#87EED9]"></div>
-                <div className="relative -top-2 overflow-hidden w-16 px-2 flex justify-center h-4 object-contain bg-[#87EED9] rounded-3xl">
+              <div className="flex flex-col justify-center items-center h-36 bg-dark hover:scale-105 duration-150 ease-in-out rounded-2xl shadow-custom w-24">
+                <div className="h-20 w-20 rounded-full border-4 border-lightTurquoise"></div>
+                <div className="relative -top-2 overflow-hidden w-16 px-2 flex justify-center h-4 object-contain bg-lightTurquoise rounded-3xl">
                   <img
                     className="w-4 h-4"
                     src="https://s3-alpha-sig.figma.com/img/fea8/d3ae/e63153e4aac87e918cc1611bbba3b28e?Expires=1713139200&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4&Signature=hk6w6lBFcKnQ9Sgo1qiS5~OmjkNworEV1MS094qwG3vQ16CPxlg5vaz1fjTUpLexe1kPXQHhimYkj6LoCfG4U8TDoNl1jLB8I6CRZSBobLO3Mev-dTBg2R2KrmNI4Wv9BvbTIK8-S~Azd0t43CMMeH7aIkyUEbIgyk-xWywZCox1Uy4BoMy3-PdjmxGBQgOJeeHWHhV8w~a4T9RZ~cosqNKpU6jr4ZE0NJwf3u9aKJQBCfA62pfTsCpFOnzSOqJQagl1t0yo1zpIdPF2eUh6trhMd5hPgxxRR871YDEsKvkopYgkGaoZdBrmGdCInlWXvs6j3DOgOcgTf9vPKxN2pA__"
@@ -85,9 +85,9 @@ export default function StudentDashboard() {
               </div>
             </div>
             <div className="flex flex-col justify-center items-center">
-              <div className="flex flex-col justify-center items-center h-36 bg-[#172935] shadow-custom hover:scale-105 duration-150 ease-in-out rounded-2xl w-24">
-                <div className="h-20 w-20 rounded-full border-4 border-[#87EED9]"></div>
-                <div className="relative -top-2 overflow-hidden w-16 px-2 flex justify-center h-4 object-contain bg-[#87EED9] rounded-3xl">
+              <div className="flex flex-col justify-center items-center h-36 bg-dark shadow-custom hover:scale-105 duration-150 ease-in-out rounded-2xl w-24">
+                <div className="h-20 w-20 rounded-full border-4 border-lightTurquoise"></div>
+                <div className="relative -top-2 overflow-hidden w-16 px-2 flex justify-center h-4 object-contain bg-lightTurquoise rounded-3xl">
                   <img
                     className="w-4 h-4"
                     src="https://s3-alpha-sig.figma.com/img/fea8/d3ae/e63153e4aac87e918cc1611bbba3b28e?Expires=1713139200&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4&Signature=hk6w6lBFcKnQ9Sgo1qiS5~OmjkNworEV1MS094qwG3vQ16CPxlg5vaz1fjTUpLexe1kPXQHhimYkj6LoCfG4U8TDoNl1jLB8I6CRZSBobLO3Mev-dTBg2R2KrmNI4Wv9BvbTIK8-S~Azd0t43CMMeH7aIkyUEbIgyk-xWywZCox1Uy4BoMy3-PdjmxGBQgOJeeHWHhV8w~a4T9RZ~cosqNKpU6jr4ZE0NJwf3u9aKJQBCfA62pfTsCpFOnzSOqJQagl1t0yo1zpIdPF2eUh6trhMd5hPgxxRR871YDEsKvkopYgkGaoZdBrmGdCInlWXvs6j3DOgOcgTf9vPKxN2pA__"
@@ -99,9 +99,9 @@ export default function StudentDashboard() {
               </div>
             </div>
             <div className="flex flex-col justify-center items-center">
-              <div className="flex flex-col justify-center items-center h-36 bg-[#172935] shadow-custom hover:scale-105 duration-150 ease-in-out rounded-2xl w-24">
-                <div className="h-20 w-20 rounded-full border-4 border-[#87EED9]"></div>
-                <div className="relative -top-2 overflow-hidden w-16 px-2 flex justify-center h-4 object-contain bg-[#87EED9] rounded-3xl">
+              <div className="flex flex-col justify-center items-center h-36 bg-dark shadow-custom hover:scale-105 duration-150 ease-in-out rounded-2xl w-24">
+                <div className="h-20 w-20 rounded-full border-4 border-lightTurquoise"></div>
+                <div className="relative -top-2 overflow-hidden w-16 px-2 flex justify-center h-4 object-contain bg-lightTurquoise rounded-3xl">
                   <img
                     className="w-4 h-4"
                     src="https://s3-alpha-sig.figma.com/img/fea8/d3ae/e63153e4aac87e918cc1611bbba3b28e?Expires=1713139200&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4&Signature=hk6w6lBFcKnQ9Sgo1qiS5~OmjkNworEV1MS094qwG3vQ16CPxlg5vaz1fjTUpLexe1kPXQHhimYkj6LoCfG4U8TDoNl1jLB8I6CRZSBobLO3Mev-dTBg2R2KrmNI4Wv9BvbTIK8-S~Azd0t43CMMeH7aIkyUEbIgyk-xWywZCox1Uy4BoMy3-PdjmxGBQgOJeeHWHhV8w~a4T9RZ~cosqNKpU6jr4ZE0NJwf3u9aKJQBCfA62pfTsCpFOnzSOqJQagl1t0yo1zpIdPF2eUh6trhMd5hPgxxRR871YDEsKvkopYgkGaoZdBrmGdCInlWXvs6j3DOgOcgTf9vPKxN2pA__"
@@ -113,9 +113,9 @@ export default function StudentDashboard() {
               </div>
             </div>
             <div className="flex flex-col justify-center items-center">
-              <div className="flex flex-col justify-center items-center h-36 bg-[#172935] shadow-custom hover:scale-105 duration-150 ease-in-out rounded-2xl w-24">
-                <div className="h-20 w-20 rounded-full border-4 border-[#87EED9]"></div>
-                <div className="relative -top-2 overflow-hidden w-16 px-2 flex justify-center h-4 object-contain bg-[#87EED9] rounded-3xl">
+              <div className="flex flex-col justify-center items-center h-36 bg-dark shadow-custom hover:scale-105 duration-150 ease-in-out rounded-2xl w-24">
+                <div className="h-20 w-20 rounded-full border-4 border-lightTurquoise"></div>
+                <div className="relative -top-2 overflow-hidden w-16 px-2 flex justify-center h-4 object-contain bg-lightTurquoise rounded-3xl">
                   <img
                     className="w-4 h-4"
                     src="https://s3-alpha-sig.figma.com/img/fea8/d3ae/e63153e4aac87e918cc1611bbba3b28e?Expires=1713139200&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4&Signature=hk6w6lBFcKnQ9Sgo1qiS5~OmjkNworEV1MS094qwG3vQ16CPxlg5vaz1fjTUpLexe1kPXQHhimYkj6LoCfG4U8TDoNl1jLB8I6CRZSBobLO3Mev-dTBg2R2KrmNI4Wv9BvbTIK8-S~Azd0t43CMMeH7aIkyUEbIgyk-xWywZCox1Uy4BoMy3-PdjmxGBQgOJeeHWHhV8w~a4T9RZ~cosqNKpU6jr4ZE0NJwf3u9aKJQBCfA62pfTsCpFOnzSOqJQagl1t0yo1zpIdPF2eUh6trhMd5hPgxxRR871YDEsKvkopYgkGaoZdBrmGdCInlWXvs6j3DOgOcgTf9vPKxN2pA__"
