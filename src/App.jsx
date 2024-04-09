7971885875b19929ae91dbe371ad6fa4bc28f532
Shreyas <shreyas.mohanty03@gmail.com>
2024-04-09 17:14:20 +0530

    Add dependencies and update routes in App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,33 @@
-import React from 'react'
-import { Routes, Route } from 'react-router-dom'
-import StudentDashboard from './StudentDashboard'
-import PrescriptionHistory from './PrescriptionHistory'
-import DoctorDetails from './DoctorDetails'
-import Ambulance from './Ambulance'
+import { Routes, Route } from "react-router-dom";
+import StudentDashboard from "./_root/student/pages/StudentDashboard.jsx";
+import PrescriptionHistory from "./_root/student/pages/PrescriptionHistory";
+import DoctorDetails from "./_root/student/pages/DoctorDetails";
+import Ambulance from "./_root/student/pages/Ambulance";
+import DoctorLayout from "./_root/doctor/DoctorLayout";
+import StudentLayout from "./_root/student/StudentLayout";
+import HostelOfficeLayout from "./_root/hostel_office/HostelOfficeLayout";
+import Login from "./_auth/forms/Login.jsx";
 import DoctorDashboard from './doctor_components/DoctorDashboard'
 
 export default function App() {
   return (
-      <Routes>
-        <Route path="/" element={<StudentDashboard></StudentDashboard>}></Route>
-        <Route path="/ph" element={<PrescriptionHistory></PrescriptionHistory>}></Route>
-        <Route path="/doc" element={<DoctorDetails></DoctorDetails>}></Route>
-        <Route path="/ambu" element={<Ambulance></Ambulance>}></Route>
-        <Route path="/docd" element={<DoctorDashboard></DoctorDashboard>}></Route>
-      </Routes>
-  )
+    <Routes>
+      <Route element={<DoctorLayout />}>
+        <Route path="/doctor/dashbard" element={<DoctorDashboard}/>
+      </Route>
+      <Route element={<StudentLayout />}>
+        <Route path="/student/dashboard" element={<StudentDashboard />} />
+        <Route path="/student/ambulance" element={<Ambulance />} />
+        <Route
+          path="/student/prescription-history"
+          element={<PrescriptionHistory />}
+        />
+        <Route path="/student/Doctor-details" element={<DoctorDetails />} />
+      </Route>
+      <Route element={<HostelOfficeLayout />}>
+        <Route path="/hostel" />
+      </Route>
+      <Route path="/login" element={<Login />} />
+    </Routes>
+  );
 }
