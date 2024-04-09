68b39aacfbc0338d80640f0870ad46abe8bfd92b
Shreyas <shreyas.mohanty03@gmail.com>
2024-04-09 09:10:45 +0530

    dashboard info added

---
+++ src/constant/index.js
@@ -0,0 +1,18 @@
+export const dashboardInformation = [
+    {
+        imgUrl: "src/assets/home2.png",
+        label: "Home"
+    },
+    {
+        imgUrl: "src/assets/task1.png",
+        label: "Tasks"
+    },
+    {
+        imgUrl: "src/assets/prescription1.png",
+        label: "Prescriptions"
+    },
+    {
+        imgUrl: "src/assets/setting2.png",
+        label: "Settings"
+    }
+]
